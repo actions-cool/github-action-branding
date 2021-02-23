86de1f80931b4d72f0f1f2d60662325e16d81fd3
ajuner <106791576@qq.com>
2021-02-23 17:48:07 +0800

    fix: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import md from './plugins/vite-plugin-md';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), vueJsx(), md()],
+  plugins: [vue(), vueJsx()],
 });
